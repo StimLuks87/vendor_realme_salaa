26e14e2423b29b39386313636478babd40d69d7e
StimLuks87 <153687700+StimLuks87@users.noreply.github.com>
2026-03-26 02:07:25 +0500

    Include blobs from F18

--- Android.mk
+++ Android.mk
@@ -3,3 +3,22 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),salaa)
+
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,81cbf792dcc38b25d026ef00d44a00ea781884b8)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,d9104d432852627ff227cf994bca11dd5eb7dc99)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,902b390975fb25deeabab3757f72308cb82e5508)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,49b305c723024fac418c906e8c1012a31c2cae82)
+$(call add-radio-file-sha1-checked,radio/cdt_engineering.img,e117a4fd0e8710102e980d3e83f37c72b8cf0596)
+$(call add-radio-file-sha1-checked,radio/gz1.img,b1349a81830de5fdcb7ab1037a388664386c7ccd)
+$(call add-radio-file-sha1-checked,radio/lk.img,7bf0b6e214f32205260bb6c2bf53e95595bc10c8)
+$(call add-radio-file-sha1-checked,radio/logo.bin,69fe09f876db5c0bc86f7cc53ead64afa2de6e3d)
+$(call add-radio-file-sha1-checked,radio/md1img.img,ce4d9a523ba76a422a3441ec693531382e20a73e)
+$(call add-radio-file-sha1-checked,radio/preloader_ufs.img,9311d559d3d594a31f4fa2c3f5fe641374286081)
+$(call add-radio-file-sha1-checked,radio/scp1.img,037eef71edd4ccc6fbe2d2dced686d5a4c0758e0)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,62cefcb4bcecc03fbf509603a9928fb3ec1911be)
+$(call add-radio-file-sha1-checked,radio/sspm_1.img,482bc3478569d9deda744d95b08a34ed2bd2c80d)
+$(call add-radio-file-sha1-checked,radio/tee1.img,f1deed4667309e1c07851d738902caa8813f79a3)
+
+endif
